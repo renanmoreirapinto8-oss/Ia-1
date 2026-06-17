481b32c5f5692f9ddee28754964f91d18a9a3559
renan <renanmoreirapinto8@gmail.com>
2026-06-16 22:36:25 -0300

    sistema de prioridades

--- Area1.py
+++ Area1.py
@@ -2,7 +2,7 @@ import tkinter as tk
 import random
 janela = tk.Tk()
 janela.title("Area 1")
-janela.geometry("1100x900")
+janela.geometry("900x700")
 canvas = tk.Canvas(janela, bg="white")
 canvas.pack(fill=tk.BOTH, expand=True)
 area_x1 = 100
@@ -27,6 +27,9 @@ class C1:
         self.velocidade_y = 0
         self.perigo = None
         self.movendo =  True
+        self.prioridade_comida = 0
+        self.prioridade_fuga = 0
+        self.prioridade_explorar = 0 
         self.fome = 100
         self.agressividade = 0
         self.medo = 0
@@ -97,6 +100,15 @@ class C1:
         if self.distancia_ate(comida) <30:
             self.fome = 100
             self.vida += 5
+            if self.vida >= 100:
+                self.vida = 100
+            self.lembrar_presa(
+                comida.x,
+                comida.y
+            )
+        if self.distancia_ate(comida2) <30:
+            self.fome = 100
+            self.vida += 5
             self.lembrar_presa(
                 comida.x,
                 comida.y
@@ -108,6 +120,10 @@ class C1:
     def detectar_comida(self):
         if self.distancia_ate(comida) < 150:
             self.alvo = comida
+        if self.distancia_ate(comida) > self.distancia_ate(comida2):
+            self.alvo = comida2
+            
+
 
     def atualizar_medo(self):
         if self.medo < 0 :
@@ -125,14 +141,15 @@ class C1:
     def atualizar_agressividade(self):
         self.agressividade = (
             (100 - self.vida) +
-            (100 - self.vida)
+            (100 - self.fome)
         ) / 2
     def explorando(self):
         if random.randint(1,20) == 1:
             self.velocidade_x = random.randint(-5,5)
             self.velocidade_y = random.randint(-5,5)
-    def observar(self,comida,ameaca):
+    def observar(self,comida,comida2,ameaca):
         distancia_comida = self.distancia_ate(comida)
+        distancia_comida = self.distancia_ate(comida2)
         distancia_ameaca = self.distancia_ate(ameaca)
         print("comida")
         distancia_comida = (
@@ -141,6 +158,12 @@ class C1:
         ) ** 0.5
         if distancia_comida < 150:
             self.alvo = comida
+        distancia_comida = (
+            (comida2.x - self.x) ** 2 +
+            (comida2.y - self.y) ** 2
+        ) ** 0.5
+        if distancia_comida < 150:
+            self.alvo = comida2
         else:
             self.alvo = None
         print("perigo")
@@ -183,6 +206,13 @@ class C1:
     def atualizar_fuga(self):
         if self.medo <= 20:
             self.explorando()
+    def atualizar_prioridades(self):
+        self.prioridade_comida = 100 - self.fome
+
+        self.prioridade_fuga = self.medo
+        
+        self.prioridade_explorar = 20
+
     def estados(self):
         if self.estado == "cacando":
             self.cacando()
@@ -192,12 +222,17 @@ class C1:
             self.fugindo()
         print(self.estado)
     def decidir(self):
-        if self.medo >= 40:
-            self.estado = "fugindo"
-        elif self.fome < 50:
-            self.estado = "cacando"
-        else:
-            self.estado = "explorando"
+        prioridades = {
+            "cacando": self.prioridade_comida,
+            "fugindo": self.prioridade_fuga,
+            "explorando": self.prioridade_explorar
+        }
+        self.estado = max(
+            prioridades,
+            key=prioridades.get
+        )
+        self.estados()
+
     
 
         self.estados() 
@@ -208,8 +243,9 @@ class C1:
         self.movimentacao()
         self.colisao()
         self.interagir()
-        self.observar(comida,ameaca)
+        self.observar(comida,comida2,ameaca)
         self.decidir()
+        self.atualizar_prioridades()
         self.atualizar_fome()
         self.detectar_comida()
         self.atualizar_fuga()
@@ -226,6 +262,83 @@ class C1:
 
 
 class presa:
+    def __init__(self, canvas, x, y):
+        self.x = x
+        self.y = y
+        self.vida = 100
+        self.velocidade_x = 0
+        self.velocidade_y = 0
+        self.velocidade = 5
+        self.velocidade_base = self.velocidade
+        self.movendo =  True
+        self.tamanho = 40
+        self.canvas = canvas
+        self.objeto = canvas.create_oval(
+            x - self.tamanho /2,
+            y - self.tamanho /2,
+            x + self.tamanho /2,
+            y + self.tamanho /2,
+            fill="gray"
+        )
+    def movimentacao(self):
+
+        if random.randint(1,20) == 1:
+            self.velocidade_x = random.randint(-5,5)
+            self.velocidade_y = random.randint(-5,5)
+
+        self.canvas.move(
+            self.objeto,
+            self.velocidade_x,
+            self.velocidade_y
+        )
+
+        self.x += self.velocidade_x
+        self.y += self.velocidade_y
+    def mudar_direcao(self):
+        self.velocidade_x = random.randint(-5,5)
+        self.velocidade_y = random.randint(-5,5)
+    def colisao(self):
+        if self.x - self.tamanho /2 <= area_x1:
+            self.velocidade_x = abs(self.velocidade_x)
+        elif self.x + self.tamanho /2 >= area_x2:
+            self.velocidade_x = -abs(self.velocidade_x)
+        if self.y - self.tamanho /2 <= area_y1:
+            self.velocidade_y = abs(self.velocidade_y)
+        elif self.y + self.tamanho /2 >= area_y2:
+            self.velocidade_y = -abs(self.velocidade_y)
+    def atualizar_colisao(self):    
+        distancia = (
+        (self.x - ia.x) ** 2 +
+        (self.y - ia.y) ** 2
+        ) ** 0.5
+        if distancia < 50:
+            if self.x > ia.x:
+                self.velocidade_x = self.velocidade
+            else:
+                self.velocidade_x = -self.velocidade
+            if self.y > ia.y:
+                self.velocidade_y = self.velocidade
+            else:
+                self.velocidade_y = -self.velocidade
+        if distancia < 30:
+            comida.x = random.randint(area_x1 + 30,area_x2 - 30)
+            comida.y = random.randint(area_y1 + 30,area_y2 - 30)
+            canvas.coords(
+                comida.objeto,
+                comida.x - comida.tamanho/2,
+                comida.y - comida.tamanho/2,
+                comida.x + comida.tamanho/2,
+                comida.y + comida.tamanho/2
+            )
+    def atualizar(self):
+
+        
+
+        self.atualizar_colisao()
+        self.movimentacao()
+        self.colisao()
+        janela.after(20, self.atualizar)
+class presa2:
     def __init__(self, canvas, x, y):
         self.x = x
         self.y = y
@@ -370,9 +483,11 @@ class ostil:
         janela.after(20, self.atualizar)
 ia = C1(canvas, 400, 300)
 comida = presa(canvas,200,200)
+comida2 = presa2(canvas,200,200)
 ameaca = ostil(canvas,200,200)
 ia.atualizar()
 comida.atualizar()
+comida2.atualizar()
 ameaca.atualizar()
 janela.mainloop()
 
